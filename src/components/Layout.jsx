2cf6fb29f1794886eee67616184dd45f09b777e4
JWhite30515 <jwhite30515@gmail.com>
2020-04-06 15:53:14 -0400

    back button for all screens

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,21 +2,24 @@ import * as React from 'react';
 
 import { IoIosHome } from 'react-icons/io';
 import { IoMdArrowRoundBack } from 'react-icons/io';
-import { Link } from 'react-router-dom';
-import { useHistory} from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
 import '../css/layout.css';
 import '../css/common.css';
 
 export default function Layout(props) {
-  const hist = useHistory();
+  const history = useHistory();
   return (
     <div>
       <div className="nav light-green-back">
+        <IoMdArrowRoundBack
+          size={'3em'}
+          onClick={()=> history.goBack() }
+          className="nav-icon clickable"
+        />
         <Link to='/'>
           <IoIosHome size={'3em'} className="nav-icon clickable"/>
         </Link>
-        <IoMdArrowRoundBack size={'3em'} onClick={()=> hist.goBack() } className="nav-icon clickable"/>
       </div>
       {props.children}
     </div>
